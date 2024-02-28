acad2501fbb6ff654cf1455320ca1ddf03e1f175
therealmarton <28adammarton28@gmail.com>
2024-02-28 15:06:55 +0100

    comm

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -2,4 +2,10 @@ SELECT *, IIF(MONTH(METTOL) IN (6,7,8), 'igen', 'nem' ) AS "NYárie"
 FROM FOGLALAS
 WHERE UGYFEL_FK =  'laszlo2'  AND GYERMEK_SZAM = 0  
 
-SELECT
+ SELECT szh.TIPUS, YEAR(f.METTOL) AS 'Év', MONTH(f.METTOL) AS 'Hónap'
+FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID
+                JOIN Szallashely szh ON  SZOBA
+
+WHERE   >=5 
+
+ GROUP BY szh.TIPUS, Válasz 4 2 kérdés
